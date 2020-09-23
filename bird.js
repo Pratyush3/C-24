4ac5ed059c9879d0d3ad50b91153ee696840985e
Pratyush3 <69083613+Pratyush3@users.noreply.github.com>
2020-09-23 20:40:14 +0530

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,31 @@
+class Birds{
+    constructor(x,y){
+        var options = {
+            'restitution':0.8,
+            'friction':0.3,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x,y,50,50,options);
+        this.w = 50;
+        this.h = 50;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        pos.x = mouseX;
+        pos.y = mouseY;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(4);
+        stroke("blue");
+        fill("red");
+        rect(0,0,50,50);
+        pop();
+    }
+   
+
+}
